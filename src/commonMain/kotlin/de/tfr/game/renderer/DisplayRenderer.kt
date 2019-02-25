adbef42c2d1e0dadbb1fc4322ca599d0d882961d
Tobse <Tobse4Git@gmail.com>
2019-02-25 23:20:56 +0100

    🎨 Add background color to timer

--- src/commonMain/kotlin/de/tfr/game/renderer/DisplayRenderer.kt
+++ src/commonMain/kotlin/de/tfr/game/renderer/DisplayRenderer.kt
@@ -1,14 +1,13 @@
 package de.tfr.game.renderer
 
 import com.soywiz.klogger.Logger
-import com.soywiz.korge.view.Graphics
-import com.soywiz.korge.view.Text
-import com.soywiz.korge.view.setText
+import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.color.RGBA
 import com.soywiz.korim.font.BitmapFont
 import com.soywiz.korim.font.readBitmapFont
 import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korma.geom.vector.rect
 import de.tfr.game.Display
 import de.tfr.game.lib.engine.Loadable
 import de.tfr.game.ui.GRAY_DARK
@@ -21,18 +20,16 @@ import de.tfr.game.util.extensions.text
 private val log = Logger("DisplayRenderer")
 
 class DisplayRenderer(val display: Display) : Loadable {
-    lateinit var font: BitmapFont
 
+    lateinit var font: BitmapFont
+    var text: Text? = null
 
     override suspend fun create() {
         font = resourcesVfs["fonts/segment7.fnt"].readBitmapFont()
     }
 
-    var text: Text? = null
-
-
-    fun Graphics.text(text: String, x: Number, y: Number, color: RGBA = Colors.WHITE): Text {
-        return this.text(text, x, y, font, 100.0, color)
+    fun Graphics.timeText(text: String, color: RGBA = Colors.WHITE): Text {
+        return this.text(text, display.x - display.width / 2 + 5, display.y - 504, font, 92.0, color)
     }
 
     var init = false
@@ -40,15 +37,15 @@ class DisplayRenderer(val display: Display) : Loadable {
         if (!init) {
             init = true
 
-            renderer.startFill(GREEN_LIGHT)
-            // renderer.rect(display.x - display.width / 2, display.y - 510, display.width, display.height)
-            renderer.endFill()
+            renderer.solidRect(display.width, display.height, GREEN_LIGHT) {
+                position(display.x - display.width / 2, display.y - 510)
+            }
 
             renderer.text("88:88", font = font)
 
-            renderer.text("88:88", display.x - display.width / 2, display.y - 510, GREEN_LIGHT2)
+            renderer.timeText("88:88", GREEN_LIGHT2)
 
-            text = renderer.text(display.getText(), display.x - display.width / 2, display.y - 510, GRAY_DARK)
+            text = renderer.timeText(display.getText(), GRAY_DARK)
 
         } else {
             val newTime = display.getText()
