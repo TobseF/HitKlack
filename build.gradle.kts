d3b63ada0b3359fcf2b33c33d9faabecbefeaa15
Tobse <Tobse4Git@gmail.com>
2019-03-06 22:15:20 +0100

    📇 Add author, title and description to metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,30 @@
+import com.soywiz.korge.gradle.GameCategory
+import com.soywiz.korge.gradle.Orientation
 import com.soywiz.korge.gradle.korauVersion
 import com.soywiz.korge.gradle.korge
 
 buildscript {
-	repositories {
-		mavenLocal()
-		maven { url = uri("https://dl.bintray.com/soywiz/soywiz") }
-		maven { url = uri("https://plugins.gradle.org/m2/") }
-		mavenCentral()
-	}
-	dependencies {
+    repositories {
+        mavenLocal()
+        maven { url = uri("https://dl.bintray.com/soywiz/soywiz") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        mavenCentral()
+    }
+    dependencies {
         classpath("com.soywiz:korge-gradle-plugin:1.1.3")
-	}
+    }
 }
 
 apply(plugin = "korge")
 
 korge {
-    id = "de.tfr.game"
+    id = "de.tfr.game.hitclack"
+    authorName = "TobseF"
+    name = "HitKlack"
+    description = "Retro console game remake of Mephisto Hit Klack"
+    gameCategory = GameCategory.ARCADE
+    orientation = Orientation.PORTRAIT
+    exeBaseName = "HitKlack"
+
     dependencyMulti("com.soywiz:korau-mp3:$korauVersion")
 }
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -3,11 +3,11 @@ import de.tfr.game.HitKlack
 import de.tfr.game.ui.DEVICE
 import de.tfr.game.util.Resolution
 
-val resolution = Resolution(width = 1080, height = 1440)
+val resolution = Resolution(width = 800, height = 1440)
 
 val debug = false
 
-suspend fun main() = Korge(width = resolution.width, height = resolution.height, bgcolor = DEVICE) {
+suspend fun main() = Korge(title = "HitClack", width = resolution.width, height = resolution.height, bgcolor = DEVICE) {
 
     val hitKlack = HitKlack(this).apply {
         create(stage)
