e1f89be3b01cdadbc8fe21083ee87ec1de452f75
Niko Schmuck <niko@nava.de>
2020-01-29 08:12:48 +0100

    Updated to latest stable KorGE and Gradle versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,16 @@
 import com.soywiz.korge.gradle.GameCategory
 import com.soywiz.korge.gradle.Orientation
-import com.soywiz.korge.gradle.korauVersion
 import com.soywiz.korge.gradle.korge
 
 buildscript {
     repositories {
         mavenLocal()
-        maven { url = uri("https://dl.bintray.com/soywiz/soywiz") }
+        maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
         maven { url = uri("https://plugins.gradle.org/m2/") }
         mavenCentral()
     }
     dependencies {
-        classpath("com.soywiz:korge-gradle-plugin:1.2.0")
+        classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:1.5.6.0")
     }
 }
 
@@ -30,5 +29,5 @@ korge {
 
     orientation = Orientation.PORTRAIT
 
-    dependencyMulti("com.soywiz:korau-mp3:$korauVersion")
+    //dependencyMulti("com.soywiz:korau-mp3:$korauVersion")
 }
